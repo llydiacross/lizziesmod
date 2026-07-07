30b3962f8f75a37d1598ca23f527f2205a9fa5e5
llydiacross <llydiacross@gmail.com>
2026-07-07 23:47:48 +0100

    fixes

--- 02_LizziesMod/Harmony/BlockJukebox.cs
+++ 02_LizziesMod/Harmony/BlockJukebox.cs
@@ -4,16 +4,36 @@ namespace LizziesMod
 {
     public class BlockJukebox : Block
     {
+
+        private BlockActivationCommand[] cmds = new BlockActivationCommand[]
+        {
+
+            new BlockActivationCommand("use", "electric_switch", true)
+        };
+
+        public override bool HasBlockActivationCommands(WorldBase _world, BlockValue _blockValue, Vector3i _blockPos, EntityAlive _entityFocusing)
+        {
+            return true;
+        }
+
+        public override BlockActivationCommand[] GetBlockActivationCommands(WorldBase _world, BlockValue _blockValue, Vector3i _blockPos, EntityAlive _entityFocusing)
+        {
+            return cmds;
+        }
+
+     
         public override string GetActivationText(WorldBase _world, BlockValue _blockValue, Vector3i _blockPos, EntityAlive _entityFocusing)
         {
             return "Use Jukebox";
         }
 
-        public override bool OnBlockActivated(WorldBase _world, Vector3i _blockPos, BlockValue _blockValue, EntityPlayerLocal _player)
+     
+        public override bool OnBlockActivated(string _commandName, WorldBase _world, Vector3i _blockPos, BlockValue _blockValue, EntityPlayerLocal _player)
         {
-            TileEntityPowered te = _world.GetTileEntity(_blockPos) as TileEntityPowered;
+            
+            if (_commandName != "use") return false;
 
-            Logger.Info("test");
+            TileEntityPowered te = _world.GetTileEntity(_blockPos) as TileEntityPowered;
 
             if (te != null && !te.IsPowered)
             {
@@ -21,9 +41,7 @@ namespace LizziesMod
                 _player.PlayOneShot("ui_denied");
                 return false;
             }
-
             JukeboxUIController.CurrentJukeboxPosition = _blockPos.ToVector3();
-
             _player.playerUI.windowManager.Open("windowJukebox", true);
 
             return true;
